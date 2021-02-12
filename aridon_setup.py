7fb9e50037af19d93cf35bb2bd57eec89b7d6d3f
bob asarous <bobsarous.git@gmail.com>
2021-02-11 19:12:38 -0500

    Reworked comments, removed unneeded ones, code is cleaner and more readable

--- aridon_setup.py
+++ aridon_setup.py
@@ -1,36 +1,37 @@
 # ? C:\Users\Bobasarous\Documents\Random Windows Stuff\Programming\PythonStuffs\aridon\aridon_setup.py
 
 # Aridon is a text based adventure based on DnD rules, classes, etc...
-# TODO Add user input that allows them to choose to see explanations for all races, classes, backgrounds, and alignments
-# TODO Finish making all the class option set ups for all the classes in game, and everything
+# TODO Add explanations for all races, classes/subclasses, backgrounds, alignments, class items, ec
+# TODO Add remaining character creation tools, class options, give players their background items, class items, pro.
+# TODO Add remaining character creation stuffs, bonds, flaws, height, weight, skin/eye color, etc...
 
 import race_descriptions
 
 
-# Setup for the player's race, size, most of the languages they will know, and their gender
+# Setup function for the player's race, size, languages, and their gender
 def race_setup():
     while True:  # Loop for user to choose their gender
-        genders = ["male", "female", "neutral/NB", "Choose your own"]
+        genders = ["male", "female", "neutral/NB", "Choose your own"]  # Genders you can choose to be
 
         print(f"Do you want to be {genders[0]}(1), {genders[1]}(2), or {genders[2]}(3), {genders[3]}(4)?")
 
         gender_choice = input().lower()  # User input for which gender they want
 
         try:  # Making sure the user input a number and converting it to a list usable set
-            gender_choice = int(gender_choice) - 1  # Turning the numbers into usable numbers for the list
+            gender_choice = int(gender_choice) - 1
 
-            if gender_choice < 0 or gender_choice > 3:  # Making sure the numbers are in the right range
+            if gender_choice < 0 or gender_choice > 3:
                 print("Please only choose numbers 1-4")
                 gender_choice = "yes"
 
             if 0 <= gender_choice <= 3:
                 while True:
-                    yes_ = "yes y ye"  # variations of yes
-                    no_ = "no n o on"  # Variations of no
+                    yes_ = "yes y ye"
+                    no_ = "no n o on"
                     if 0 <= gender_choice <= 2:
                         print(f"You have chosen {genders[gender_choice]}, do you wish to continue? (yes or no)")
 
-                        gender_correct = input().lower()  # User input for making sure they didn't change their mind
+                        gender_correct = input().lower()
 
                         if gender_correct in yes_:
                             print(f"You have chosen {genders[gender_choice]} as your gender, good luck!")
@@ -40,18 +41,17 @@ def race_setup():
                         if gender_correct in no_:
                             print("Alright, please try again.")
                             break
-                        else:  # Making sure they only type yes or no
+                        else:
                             print("Please only type yes or no!")
 
                     if gender_choice == 3:
-                        yes_ = "yes y ye"  # variations of yes
-                        no_ = "no n o on"  # Variations of no
+                        yes_ = "yes y ye"
+                        no_ = "no n o on"
                         print("Please type your own gender")
-                        gender_choice = input()  # Allowing the player to choose and type their own gender
+                        gender_choice = input()
                         print(f"You have chosen {gender_choice} as your gender are you sure you wish to continue?")
-                        # Showing the user what they typed and making sure they want it
 
-                        gender_correct = input().lower()  # Input for users to tell if they are sure they choose right
+                        gender_correct = input().lower()
 
                         if gender_correct in yes_:
                             print(f"You have chosen {gender_choice}, good luck!")
@@ -60,7 +60,7 @@ def race_setup():
                         if gender_correct in no_:
                             print("Alright, please try again.")
                             break
-                        else:  # Making sure they only type yes or no
+                        else:
                             print("Please only type yes or no!")
 
         except ValueError:  # In case they did not type a number
@@ -74,16 +74,9 @@ def race_setup():
 
     while True:  # Loop for choosing their race
 
-        # Used to access the races_dict variable, and makes it easier to assign race here, only making the user type -
-        # numbers, instead of typing a whole word, which they could misspell, and then I'd have to make if statements -
-        # for each race, instead of just assigning it based off of numbers and such, much easier to have both variables
-        races = ["Dwarf", "Elf", "Human", "Half-Orc", "Tiefling"]
-
-        # All the races in the game, make sure they line up with their racial counterpart, will make it easier to -
-        # grab which lang you want based on the race the user choose
-        languages = ["Dwarvish", "Elvish", "Common", "Orcish", "Infernal"]
-        # All the sizes in the game
-        sizes = ["Tiny", "Small", "Medium", "Large", "Huge", "Gargantuan"]
+        races = ["Dwarf", "Elf", "Human", "Half-Orc", "Tiefling"]  # Races in game
+        languages = ["Dwarvish", "Elvish", "Common", "Orcish", "Infernal"]  # Langs the races can know
+        sizes = ["Tiny", "Small", "Medium", "Large", "Huge", "Gargantuan"]  # Sizes in game
 
         print("Please choose a race for your character.")
         print(f"Type 1 for {races[0]}")
@@ -92,31 +85,37 @@ def race_setup():
         print(f"Type 4 for {races[3]}")
         print(f"Type 5 for {races[4]}")
 
-        race_choice = input()  # User input for which race they want
+        race_choice = input()
 
         try:  # Making sure the user input a number and converting it to a list usable set
             race_choice = int(race_choice) - 1
 
-            if race_choice < 0 or race_choice > 4:  # Making sure they only used number in proper range
+            if race_choice < 0 or race_choice > 4:
                 print("Please only type numbers between and including 1 through 5!")
 
             if 0 <= race_choice <= 4:
                 print(f"You have chosen {races[race_choice]} here is a little bit about them")
                 print(race_descriptions.race_(race_choice))
                 print("Do you wish to continue (yes or no)")
-                yes_ = "yes y ye"  # variations of yes
-                no_ = "no n o on"  # Variations of no
-
-                if 0 <= race_choice <= 4:  # Making sure they choose the correct numbers, and then generates users size
-                    size_choice = sizes[2]
+                yes_ = "yes y ye"
+                no_ = "no n o on"
 
                 while True:
                     race_correct = input().lower()  # User input for making sure they didn't change their minds
 
                     if race_correct in yes_:
+                        size_choice = sizes[2]
                         print(f"You have chosen {races[race_choice]} as your race, good luck!")
-                        lang_choice = [languages[race_choice], languages[3]]  # Generates users lang based on their race
-                        print(f"You know the languages {lang_choice}")
+                        if races[race_choice] == races[2]:
+                            # Generates your language if you choose human (humans as a species only gain common
+                            # But then they also gain one of your choice, so they still get to know 2 just you choose
+                            lang_choice = [languages[3]]
+                            print(f"Your race knows the languages {lang_choice}")
+                            race_choice = races[race_choice]  # Generates chosen race if they choose human
+                            race_correct = "yes"
+                            break
+                        lang_choice = [languages[race_choice], languages[2]]  # Generates users lang based on their race
+                        print(f"Your race knows the languages {lang_choice}")
                         race_choice = races[race_choice]  # Generates the race they chose
                         race_correct = "yes"
                         break
@@ -124,7 +123,7 @@ def race_setup():
                         print("Please choose again then.")
                         race_correct = "no"
                         break
-                    else:  # Making sure they only type yes or no
+                    else:
                         print("Please only type yes or no.")
 
         except ValueError:  # In case they did not type a number
@@ -141,41 +140,38 @@ def race_setup():
     return gender_choice, lang_choice, race_choice, size_choice
 
 
-def build_setup():  # The setup for the PC's alignment, background, build (class), health, and level
+def player_class_setup():  # The setup for the PC's alignment, background, build (class), health, and level
 
     while True:
-        # All the classes in the game
-        classes_ = ["Bard", "Cleric", "Druid", "Paladin", "Warlock"]
-        # The health values for each class MAKE SURE THIS IS ALIGNED with ^
-        health_values = [8, 8, 8, 10, 8]
+        game_classes = ["Bard", "Cleric", "Druid", "Paladin", "Warlock"]  # Classes in the game
+        health_values = [8, 8, 8, 10, 8]  # HP for each class (aligned with class on list index)
 
         print("Please choose a class.")
-        print(f"Type 1 for {classes_[0]}")
-        print(f"Type 2 for {classes_[1]}")
-        print(f"Type 3 for {classes_[2]}")
-        print(f"Type 4 for {classes_[3]}")
-        print(f"Type 5 for {classes_[4]}")
+        print(f"Type 1 for {game_classes[0]}")
+        print(f"Type 2 for {game_classes[1]}")
+        print(f"Type 3 for {game_classes[2]}")
+        print(f"Type 4 for {game_classes[3]}")
+        print(f"Type 5 for {game_classes[4]}")
 
-        build_choice = input()  # User input to choose which class they want
+        player_class_choice = input()
 
         try:  # Making sure the user input a number and converting it to a list usable set
-            build_choice = int(build_choice) - 1
+            player_class_choice = int(player_class_choice) - 1
 
-            if build_choice < 0 or build_choice > 4:  # Making sure they choose the right numbers
+            if player_class_choice < 0 or player_class_choice > 4:
                 print("Please only type numbers 1 through 5")
 
-            if 0 <= build_choice <= 4:
+            if 0 <= player_class_choice <= 4:
                 while True:
-                    print(f"You have chosen {classes_[build_choice]} as your class, do you wish to continue?(yes or no)"
-                          f"")
-                    yes_ = "yes y ye"  # variations of yes
-                    no_ = "no n o on"  # Variations of no
+                    print(f"You have chosen {game_classes[player_class_choice]} as your class, do you wish to continue?(yes or no)")
+                    yes_ = "yes y ye"
+                    no_ = "no n o on"
 
-                    build_correct = input().lower()  # Making sure the user didn't change their mind
+                    build_correct = input().lower()
 
                     if build_correct in yes_:
-                        health_choice = health_values[build_choice]
-                        build_choice = classes_[build_choice]
+                        health_choice = health_values[player_class_choice]
+                        player_class_choice = game_classes[player_class_choice]
                         print("Good luck!")
                         build_correct = "yes"
                         break
@@ -196,9 +192,8 @@ def build_setup():  # The setup for the PC's alignment, background, build (class
 
     while True:  # Allowing the user to choose their alignment
 
-        # All the alignments in the game
         alignments = ["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", "True Neutral",
-                      "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil"]
+                      "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil"]  # DnD alignments
 
         print("Please choose an alignment")
         print(f"1 for {alignments[0]}")
@@ -211,28 +206,27 @@ def build_setup():  # The setup for the PC's alignment, background, build (class
         print(f"8 for {alignments[7]}")
         print(f"9 for {alignments[8]}")
 
-        alignment_choice = input()  # User input to choose the alignments
+        alignment_choice = input()
 
         try:  # Making sure the user input a number and converting it to a list usable set
             alignment_choice = int(alignment_choice) - 1
 
-            if alignment_choice < 0 or alignment_choice > 8:  # Making sure the user used numbers 1 through 9
+            if alignment_choice < 0 or alignment_choice > 8:
                 print("Only numbers 1 through 9 please!")
 
             if 0 <= alignment_choice <= 8:
-                print(f"You have chosen {alignments[alignment_choice]} as your alignment do you wish to continue? ("
-                      f"yes or no)")
+                print(f"You have chosen {alignments[alignment_choice]} as your alignment do you wish to continue? (yes or no)")
 
-                while True:  # Giving the player a chance to change their minds
-                    yes_ = "yes y ye"  # variations of yes
-                    no_ = "no n o on"  # Variations of no
+                while True:
+                    yes_ = "yes y ye"
+                    no_ = "no n o on"
 
                     alignment_correct = input().lower()
 
                     if alignment_correct in yes_:
                         print("Good luck!")
-                        alignment_choice = alignments[alignment_choice]  # Setting the variable to the value we want -
-                        alignment_correct = "yes"  # so we can return it
+                        alignment_choice = alignments[alignment_choice]  # Generating players alignment
+                        alignment_correct = "yes"
                         break
                     if alignment_correct in no_:
                         print("Then please choose again.")
@@ -254,8 +248,7 @@ def build_setup():  # The setup for the PC's alignment, background, build (class
         backgrounds = ["Acolyte", "Charlatan", "Criminal", "Entertainer", "Folk Hero", "Guild Artisan", "Hermit",
                        "Noble", "Outlander", "Sage", "Sailor", "Soldier", "Urchin"]
 
-        print("Please choose a background, if you want further explanation on backgrounds 'The Players Handbook' is a "
-              "great place to look")
+        print("Please choose a background, if you want further explanation on backgrounds 'The Players Handbook' is a great place to look")
         print(f"1 for {backgrounds[0]}")
         print(f"2 for {backgrounds[1]}")
         print(f"3 for {backgrounds[2]}")
@@ -270,7 +263,7 @@ def build_setup():  # The setup for the PC's alignment, background, build (class
         print(f"12 for {backgrounds[11]}")
         print(f"13 for {backgrounds[12]}")
 
-        background_choice = input().lower()  # Users input for what background they want to choose
+        background_choice = input().lower()
 
         try:
             background_choice = int(background_choice) - 1
@@ -282,14 +275,14 @@ def build_setup():  # The setup for the PC's alignment, background, build (class
                 print(f"You chose {backgrounds[background_choice]}, do you wish to continue? (yes or no)")
 
                 while True:
-                    yes_ = "yes y ye"  # variations of yes
-                    no_ = "no n o on"  # Variations of no
+                    yes_ = "yes y ye"
+                    no_ = "no n o on"
 
                     bg_correct = input().lower()
 
                     if bg_correct in yes_:
                         print("Good luck!")
-                        background_choice = backgrounds[background_choice]
+                        background_choice = backgrounds[background_choice] # Generating players background
                         bg_correct = "yes"
                         break
                     if bg_correct in no_:
@@ -298,7 +291,7 @@ def build_setup():  # The setup for the PC's alignment, background, build (class
                     else:
                         print("Please only yes or no")
 
-        except ValueError:
+        except ValueError:  # Making sure player typed a number
             print("Please only type numbers")
 
         try:
@@ -307,7 +300,8 @@ def build_setup():  # The setup for the PC's alignment, background, build (class
         except UnboundLocalError:
             pass
 
-    return alignment_choice, background_choice, build_choice, health_choice
+    # Returning all info based on class and such
+    return alignment_choice, background_choice, player_class_choice, health_choice
 
 
 def build_options():
@@ -320,12 +314,12 @@ def build_options():
 # p_alignment, p_background, p_bonds, p_flaws, p_gender, p_height, p_ideals, p_lang, p_lvl, p_race, p_size, p_weight
 # p_c_features, p_r_traits, p_attacks, p_bg_options, p_build, p_health, p_proficiencies
 gender, lang_choices, race, size = race_setup()
-alignment, bg, build, health = build_setup()
+alignment, bg, build, health = player_class_setup()
 
-lang_str = ""
+lang_str = ""  # making it so when PC's with more than two languages come in here they are properly handled
 lang_len = len(lang_choices)
 for x in range(lang_len):
-    lang_len = lang_len - 1
+    lang_len -= lang_len
     lang_str = lang_str.__add__(f"{lang_choices[lang_len]}")
 
 
@@ -338,3 +332,4 @@ with open("class_options.txt", "w") as f:
             f"languages={lang_str}" + "\n"
             f"race={race}" + "\n"
             f"size={size}" + "\n")
+
